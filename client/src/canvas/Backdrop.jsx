50bf0387004ec80830f1fe7c07507f32cc13e308
Aditya <holaaditya123@gmail.com>
2024-06-25 12:25:07 +0530

    Fixed a bug

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -6,25 +6,25 @@ const Backdrop = () => {
 
   return (
     <AccumulativeShadows
+      position={[0, 0, -0.14]}
       ref={shadows}
       temporal
       frames={60}
       alphaTest={0.85}
-      // scale={10}
+      scale={10}
       rotation={[Math.PI / 2, 0, 0]}
-      position={[0, 0, -0.14]}
     >
       <RandomizedLight
         amount={4}
         radius={9}
-        intensity={0.55}
+        intensity={1.5}
         ambient={0.25}
         position={[5, 5, -10]}
       />
       <RandomizedLight
         amount={4}
         radius={5}
-        intensity={0.25}
+        intensity={1.25}
         ambient={0.55}
         position={[-5, 5, -9]}
       />
