3f1b17ef6291000732a9e268e3939238a322fa17
Aditya <holaaditya123@gmail.com>
2024-06-25 11:20:00 +0530

    Trying to fix openAI bugs but still no luck ;(

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,9 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-
-import OpenAI from "openai";
-
-const openai = new OpenAIApi(config);
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
@@ -12,6 +9,7 @@ const router = express.Router();
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" });
