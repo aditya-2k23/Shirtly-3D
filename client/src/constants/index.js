acfb62a7da1c65309497d706d401cbc36de6dbec
Aditya <holaaditya123@gmail.com>
2024-06-25 16:46:47 +0530

    Exporting the socialLinks

---
+++ client/src/constants/index.js
@@ -0,0 +1,32 @@
+export const socials = [
+  {
+    id: "0",
+    title: "Twitter",
+    url: "https://twitter.com/Tema_roon",
+  },
+  {
+    id: "1",
+    title: "BuyMeACoffee",
+    url: "https://buymeacoffee.com/temaroon",
+  },
+  {
+    id: "2",
+    title: "LinkedIn",
+    url: "https://linkedin.com/in/aditya-2k23",
+  },
+  {
+    id: "3",
+    title: "GitHub",
+    url: "https://github.com/aditya-2k23",
+  },
+  {
+    id: "4",
+    title: "Instagram",
+    url: "https://instagram.com/aditya.found",
+  },
+  {
+    id: "5",
+    title: "YouTube",
+    url: "https://youtube.com/@TemaroonGaming",
+  },
+];
